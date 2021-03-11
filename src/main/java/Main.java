41e4561d0effed2b88ad2d9c24391ef7a9afe64f
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2021-03-11 23:13:55 +0100

    quickstart

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class Main {
     public static void main(String[] args) throws UnirestException {
-        Quickstart quickstart = new Quickstart();
-        quickstart.submitAudioFileForTranscription();
+//        Quickstart quickstart = new Quickstart();
+//        quickstart.submitAudioFileForTranscription();
+//        quickstart.getTranscriptionResult();
     }
 }
--- src/main/java/Quickstart.java
+++ src/main/java/Quickstart.java
@@ -1,13 +1,10 @@
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Quickstart {
 
     private static final String YOUR_API_TOKEN = "615d6b6573404fa9a1d7cc340b4023fb";
+    private static final String YOUR_TRANSCRIPT_ID_HERE = "ihtczepza-8f3a-4e78-bb2c-eef88b3a4f2d";
 
     public void submitAudioFileForTranscription() throws UnirestException {
 
@@ -16,6 +13,18 @@ public class Quickstart {
                 .header("authorization", YOUR_API_TOKEN)
                 .header("content-type", "application/json")
                 .body("{\"audio_url\":\"https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3\"}")
+                .asString()
+                .getBody();
+
+        System.out.println(responseJson);
+    }
+
+    public void getTranscriptionResult() throws UnirestException {
+
+        String responseJson = Unirest
+                .get("https://api.assemblyai.com/v2/transcript/" + YOUR_TRANSCRIPT_ID_HERE)
+                .header("authorization", YOUR_API_TOKEN)
+                .header("content-type", "application/json")
                 .asString()
                 .getBody();
 
