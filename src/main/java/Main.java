c8132bdb477b17cb6f75820738329206491d8883
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2021-03-13 11:09:17 +0100

    print signed url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,27 +16,27 @@ public class Main {
         boostingAccuracyOfKeyworlds.boostKeywords();
         boostingAccuracyOfKeyworlds.controlTheWeightOfBoost();
 
-        SelectingModelThatBetterMatchesData selectingModelThatBetterMatchesData=new SelectingModelThatBetterMatchesData();
+        SelectingModelThatBetterMatchesData selectingModelThatBetterMatchesData = new SelectingModelThatBetterMatchesData();
         selectingModelThatBetterMatchesData.choosingAnAcousticModel();
         selectingModelThatBetterMatchesData.choosingLanguageModel();
 
-        GettingSpeakerLabels gettingSpeakerLabels=new GettingSpeakerLabels();
+        GettingSpeakerLabels gettingSpeakerLabels = new GettingSpeakerLabels();
         gettingSpeakerLabels.submitAudioFileWithSpeakerLabel();
         gettingSpeakerLabels.getTranscriptionResult();
 
-        GettingWebhooksWhenTranscriptionAreDone gettingWebhooksWhenTranscriptionAreDone=new GettingWebhooksWhenTranscriptionAreDone();
+        GettingWebhooksWhenTranscriptionAreDone gettingWebhooksWhenTranscriptionAreDone = new GettingWebhooksWhenTranscriptionAreDone();
         gettingWebhooksWhenTranscriptionAreDone.specifyWebhookURL();
         gettingWebhooksWhenTranscriptionAreDone.getTranscriptionResult();
 
-        TurnOffAutomaticPunctuationAndCasing turnOffAutomaticPunctuationAndCasing =new TurnOffAutomaticPunctuationAndCasing();
+        TurnOffAutomaticPunctuationAndCasing turnOffAutomaticPunctuationAndCasing = new TurnOffAutomaticPunctuationAndCasing();
         turnOffAutomaticPunctuationAndCasing.submitAudioFileForTranscriptionAndEnableAutoHighlight();
 
-        RedactPIIFromText redactPIIFromText=new RedactPIIFromText();
+        RedactPIIFromText redactPIIFromText = new RedactPIIFromText();
         redactPIIFromText.redactPIIFromTranscription();
         redactPIIFromText.customizeHowRedactedPIIIsReplaced();
         redactPIIFromText.redactPIIFromAudio();
 
-        ExportingTranscript exportingTranscript=new ExportingTranscript();
+        ExportingTranscript exportingTranscript = new ExportingTranscript();
         exportingTranscript.checkStatus();
         exportingTranscript.exportAsVTT();
         exportingTranscript.exportAsSRT();
@@ -51,7 +51,8 @@ public class Main {
         DeleteTranscription deleteTranscription = new DeleteTranscription();
         deleteTranscription.delete();
 
-        GivingTemporaryAccessGCS givingTemporaryAccessGCS=new GivingTemporaryAccessGCS();
-        givingTemporaryAccessGCS.createPreSignedURL();
+        GivingTemporaryAccessGCS givingTemporaryAccessGCS = new GivingTemporaryAccessGCS();
+        String url = givingTemporaryAccessGCS.createPreSignedURL();
+        System.out.println(url);
     }
 }
